074cd19363490597b56095de82f1e5d661c2cb74
Piplot26 <munizalejandro681@gmail.com>
2022-07-04 18:53:24 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//  console.log('Hola Mundo');
+
+
+let a = 10,
+ b = 20, 
+ c = "Hola ", 
+ d = "Spiderman", 
+ x = a + b
+
+
+ const saludo = c + d;
+ 
+
+// console.log('%c Mis Variables', 'color:blue; font-weigth: bold');
+// console.log({a});
+// console.warn({b}); 
+// console.error({c});
+
+c = "Hola de nuevo";
+
+// console.table({a, b, c, d, x});
+
